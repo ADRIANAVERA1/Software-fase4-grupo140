fdab4a44cd33ca335dd1d74666a1b09d3a7fb67e
Edilma1293 <esepulvedag@unadvirtual.edu.co>
2026-05-06 12:03:09 -0500

    Implement main menu for reservation system
    Implementa el menú principal del sistema de reservas, permitiendo al usuario crear reservas mediante el registro de clientes, la selección de servicios y la asignación de horas de uso.
    El sistema procesa automáticamente la reserva, calcula su costo según el servicio seleccionado y muestra la información correspondiente.
    Además, incorpora manejo de excepciones y registro de errores en un archivo de logs para garantizar un control adecuado durante la ejecución.

--- main.py
+++ main.py
@@ -1,1 +1,68 @@
+from cliente import Cliente
+from servicio import ReservaSala, AlquilerEquipo, AsesoriaEspecializada
+from reserva import Reserva
+from excepciones import ClienteError, ServicioError, ReservaError
 
+def registrar_log(error):
+    with open("logs.txt", "a") as archivo:
+        archivo.write(str(error) + "\n")
+
+print(" SISTEMA SOFTWARE FJ ")
+
+while True:
+    try:
+        print("\n MENÚ PRINCIPAL ")
+        print("1. Crear reserva")
+        print("2. Salir")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+
+            # CLIENTE
+            nombre = input("Ingrese el nombre del cliente: ")
+            documento = input("Ingrese el documento: ")
+            cliente = Cliente(nombre, documento)
+
+            # SERVICIO
+            print("\nSERVICIOS DISPONIBLES")
+            print("1. Sala VIP: 50.000 COP/hora")
+            print("2. Alquiler Equipo: 30.000 COP/hora + 20.000 COP cargo fijo")
+            print("3. Asesoría: 60.000 COP/hora + 15% adicional")
+
+            op_servicio = input("Seleccione el servicio: ")
+
+            horas = int(input("Ingrese horas de uso: "))
+
+            if op_servicio == "1":
+                servicio = ReservaSala("Sala VIP", 50000)
+
+            elif op_servicio == "2":
+                servicio = AlquilerEquipo("Equipo", 30000)
+
+            elif op_servicio == "3":
+                servicio = AsesoriaEspecializada("Asesoria", 60000)
+
+            else:
+                print("Servicio inválido")
+                continue
+
+            # RESERVA
+            reserva = Reserva(cliente, servicio, horas)
+            reserva.confirmar()
+
+            print("\n✔ RESERVA EXITOSA")
+            print("Cliente:", cliente)
+            print("Estado:", reserva.estado)
+            print("Total:", reserva.procesar())
+
+        elif opcion == "2":
+            print("Saliendo del sistema...")
+            break
+
+        else:
+            print("Opción inválida")
+
+    except Exception as e:
+        registrar_log(e)
+        print(" Error ", e)
